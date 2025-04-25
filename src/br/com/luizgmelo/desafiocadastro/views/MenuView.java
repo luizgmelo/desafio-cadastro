eae21eae538757b083f79ac8ab12b15c45e9fd99
Luiz Guilherme <luizgmelo64@gmail.com>
2025-04-25 14:24:51 -0300

    refac: menu view

--- src/br/com/luizgmelo/desafiocadastro/views/MenuView.java
+++ src/br/com/luizgmelo/desafiocadastro/views/MenuView.java
@@ -4,6 +4,7 @@ import br.com.luizgmelo.desafiocadastro.controllers.MenuController;
 import br.com.luizgmelo.desafiocadastro.models.Pet;
 import br.com.luizgmelo.desafiocadastro.models.PetSex;
 import br.com.luizgmelo.desafiocadastro.models.PetType;
+import br.com.luizgmelo.desafiocadastro.services.InputService;
 
 import java.util.*;
 
@@ -16,85 +17,32 @@ public class MenuView {
         this.menuController = new MenuController();
     }
 
-    public void showMenu() {
-        int option = -1;
-        do {
-            System.out.println("\n==== Sistema de Cadastro de Pet ====\n");
-            System.out.println("1. Cadastrar um novo pet");
-            System.out.println("2. Alterar os dados do pet cadastrado");
-            System.out.println("3. Deletar um pet cadastrado");
-            System.out.println("4. Listar todos os pets cadastrados");
-            System.out.println("5. Listar pets por algum critério (idade, nome, raça)");
-            System.out.println("6. Sair");
-            System.out.print("Digite uma das opções acima: ");
+    public int menu() {
+        System.out.println("\n==== Sistema de Cadastro de Pet ====\n");
+        System.out.println("1. Cadastrar um novo pet");
+        System.out.println("2. Alterar os dados do pet cadastrado");
+        System.out.println("3. Deletar um pet cadastrado");
+        System.out.println("4. Listar todos os pets cadastrados");
+        System.out.println("5. Listar pets por algum critério (idade, nome, raça)");
+        System.out.println("6. Sair");
+        System.out.print("Digite uma das opções acima: ");
 
-            try {
-                option = scanner.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("\nDigite apenas um número entre 1-6");
-                continue;
-            } finally {
-                scanner.nextLine();
-            }
+        int option = -1;
+
+        return option;
+    }
+
+
+    public void showMenu() {
+        int option;
+        do {
+            menu();
+
+            option = InputService.readInt(scanner);
 
             switch (option) {
                 case 1:
-                    System.out.println("\n=== Cadastro do Pet ===");
-                    // 1. Qual o nome e sobrenome do pet?
-                    System.out.print(menuController.showNextQuestion());
-                    String petName = scanner.nextLine();
-                    menuController.validateName(petName, "nome do pet");
-
-                    // 2. Qual o tipo do pet (Cachorro/Gato)?
-                    System.out.print(menuController.showNextQuestion());
-                    String type = scanner.nextLine();
-                    PetType petType = menuController.validateType(type);
-
-                    // 3. Qual o sexo do animal?
-                    System.out.print(menuController.showNextQuestion());
-                    String sex = scanner.nextLine();
-                    PetSex petSex = menuController.validateSex(sex);
-
-                    //  4 - Qual o nome da Rua que ele foi encontrado?
-                    System.out.print(menuController.showNextQuestion());
-                    String petStreet = scanner.nextLine();
-                    menuController.validateStreetName(petStreet);
-
-                    // 5 - Qual o número da casa?
-                    System.out.print(menuController.showNextQuestion());
-                    String petHouseNumber = scanner.nextLine();
-                    petHouseNumber = menuController.validateHouseNumber(petHouseNumber);
-
-                    // 6 - Qual a cidade?
-                    System.out.print(menuController.showNextQuestion());
-                    String petCity = scanner.nextLine();
-                    menuController.validateName(petCity, "nome da cidade");
-
-                    // 7. Qual a idade aproximada do pet?
-                    System.out.print(menuController.showNextQuestion());
-                    String petAge = scanner.nextLine();
-                    petAge = menuController.validateAge(petAge);
-
-                    // 8. Qual o peso em kilos aproximado do pet?
-                    System.out.print(menuController.showNextQuestion());
-                    String petWeight = scanner.nextLine();
-                    petWeight = menuController.validateWeight(petWeight);
-
-                    // 9. Qual a raça do pet?
-                    System.out.print(menuController.showNextQuestion());
-                    String petBreed = scanner.nextLine();
-                    menuController.validateName(petBreed, "raça");
-
-                    boolean isCreated = menuController.addPet(petName, petType, petSex,
-                            petStreet, petHouseNumber, petCity,
-                            petAge, petWeight, petBreed);
-
-                    if (isCreated) {
-                        System.out.println("\n Cadastro realizado com sucesso!");
-                    } else {
-                        System.out.println("\n Não foi possível fazer o cadastro");
-                    }
-
+                    registerPet();
                     break;
                 case 2:
                     // TODO Implementar alterar cadastro de pet
@@ -106,66 +54,109 @@ public class MenuView {
                     // TODO Implementar listar todos os pets
                     break;
                 case 5:
-                    // TODO Implementar listar pets por critérios
-                    Map<String, String> criterias = new HashMap<>();
-                    List<String> options = Arrays.asList("Nome", "Sexo", "Idade", "Peso", "Raça", "Endereço");
-
-                    System.out.print("Qual o tipo do pet? (Gato/Cachorro)? ");
-                    PetType petTypeSearch = menuController.validateType(scanner.nextLine());
-
-                    int criteriaCount = 1;
-                    while (true) {
-                        showSearchMenu(options);
-                        System.out.print("Escolha o " + (criteriaCount) + "° critério: ");
-                        int criteria;
-                        try {
-                            criteria = scanner.nextInt();
-
-                            if (criteria < 1 || criteria > 6) {
-                                System.out.println("\nEscolha uma opção entre 1-6");
-                                continue;
-                            }
-
-                        } catch (RuntimeException e) {
-                            System.out.println("\nEscolha uma opção entre 1-6");
-                            continue;
-                        } finally {
-                            scanner.nextLine();
-                        }
-
-                        System.out.print("Digite o valor de " + options.get(criteria - 1) + ": ");
-                        String value = scanner.nextLine();
-                        // TODO Validate Criteria from user above
-
-                        criterias.put(options.get(criteria - 1), value);
-
-                        if (criterias.size() == 1) {
-                            System.out.print("Deseja selecionar mais um critério? (S/N)");
-                            String res = scanner.nextLine();
-
-                            if (res.charAt(0) == 'N' || res.charAt(0) == 'n') {
-                                break;
-                            }
-                        }
-
-                        if (criterias.size() == 2) {
-                            break;
-                        }
-
-                        criteriaCount++;
-                    }
-
-                    List<Pet> petFiltered = menuController.searchPet(petTypeSearch, criterias);
-
-                    showPetList(petFiltered);
+                    searchPet();
                     break;
                 case 6:
                     break;
-                default:
-                    System.out.println("\nDigite apenas um número entre 1-6");
             }
 
         } while (option != 6);
+    }
+
+    private void searchPet() {
+        Map<String, String> criterias = new HashMap<>();
+        List<String> options = Arrays.asList("Nome", "Sexo", "Idade", "Peso", "Raca", "Endereco");
+
+        System.out.print("Qual o tipo do pet? (Gato/Cachorro)? ");
+        PetType petTypeSearch = menuController.validateType(scanner.nextLine());
+
+        do {
+            showSearchMenu(options);
+
+            System.out.print("Escolha o " + (criterias.size() + 1) + "° critério: ");
+            int criteria = InputService.readInt(scanner);
+
+            if (criteria >= 1 && criteria <= 6) {
+                String option = options.get(criteria - 1);
+                System.out.print("Digite o valor de " + option + ": ");
+                String value = scanner.nextLine();
+                menuController.validateValue(value, option);
+
+                criterias.put(option, value);
+
+                if (criterias.size() == 1) {
+                    System.out.print("Deseja selecionar mais um critério? (S/N)");
+                    String res = scanner.nextLine();
+
+                    if (res.charAt(0) == 'N' || res.charAt(0) == 'n') {
+                        break;
+                    }
+                }
+            }
+
+        } while (criterias.size() != 2);
+
+        List<Pet> petFiltered = menuController.searchPet(petTypeSearch, criterias);
+
+        showPetList(petFiltered);
+    }
+
+    private void registerPet() {
+        System.out.println("\n=== Cadastro do Pet ===");
+        // 1. Qual o nome e sobrenome do pet?
+        System.out.print(menuController.showNextQuestion());
+        String petName = scanner.nextLine();
+        menuController.validateName(petName, "nome do pet");
+
+        // 2. Qual o tipo do pet (Cachorro/Gato)?
+        System.out.print(menuController.showNextQuestion());
+        String type = scanner.nextLine();
+        PetType petType = menuController.validateType(type);
+
+        // 3. Qual o sexo do animal?
+        System.out.print(menuController.showNextQuestion());
+        String sex = scanner.nextLine();
+        PetSex petSex = menuController.validateSex(sex);
+
+        //  4 - Qual o nome da Rua que ele foi encontrado?
+        System.out.print(menuController.showNextQuestion());
+        String petStreet = scanner.nextLine();
+        menuController.validateStreetName(petStreet);
+
+        // 5 - Qual o número da casa?
+        System.out.print(menuController.showNextQuestion());
+        String petHouseNumber = scanner.nextLine();
+        petHouseNumber = menuController.validateHouseNumber(petHouseNumber);
+
+        // 6 - Qual a cidade?
+        System.out.print(menuController.showNextQuestion());
+        String petCity = scanner.nextLine();
+        menuController.validateName(petCity, "nome da cidade");
+
+        // 7. Qual a idade aproximada do pet?
+        System.out.print(menuController.showNextQuestion());
+        String petAge = scanner.nextLine();
+        petAge = menuController.validateAge(petAge);
+
+        // 8. Qual o peso em kilos aproximado do pet?
+        System.out.print(menuController.showNextQuestion());
+        String petWeight = scanner.nextLine();
+        petWeight = menuController.validateWeight(petWeight);
+
+        // 9. Qual a raça do pet?
+        System.out.print(menuController.showNextQuestion());
+        String petBreed = scanner.nextLine();
+        menuController.validateName(petBreed, "raça");
+
+        boolean isCreated = menuController.addPet(petName, petType, petSex,
+                petStreet, petHouseNumber, petCity,
+                petAge, petWeight, petBreed);
+
+        if (isCreated) {
+            System.out.println("\n Cadastro realizado com sucesso!");
+        } else {
+            System.out.println("\n Não foi possível fazer o cadastro");
+        }
     }
 
     private void showPetList(List<Pet> pets) {
