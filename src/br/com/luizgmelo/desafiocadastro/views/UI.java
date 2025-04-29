5f4e8eacc164f64cf193bcc05df5b521fa7e8956
Luiz Guilherme <luizgmelo64@gmail.com>
2025-04-29 16:53:58 -0300

    refac: remove crud operations classes view and create ui class for it

---
+++ src/br/com/luizgmelo/desafiocadastro/views/UI.java
@@ -0,0 +1,276 @@
+package br.com.luizgmelo.desafiocadastro.views;
+
+import br.com.luizgmelo.desafiocadastro.controllers.MenuController;
+import br.com.luizgmelo.desafiocadastro.models.Pet;
+import br.com.luizgmelo.desafiocadastro.models.PetSex;
+import br.com.luizgmelo.desafiocadastro.models.PetType;
+import br.com.luizgmelo.desafiocadastro.services.InputService;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class UI {
+    private final Scanner scanner;
+    private final PetController petController;
+
+    public UI() {
+        this.scanner = new Scanner(System.in);
+        this.petController = new PetController();
+    }
+
+    public void showMainMenu() {
+        System.out.println("\n==== Sistema de Cadastro de Pet ====\n");
+        System.out.println("1. Cadastrar um novo pet");
+        System.out.println("2. Listar pets por algum critério (idade, nome, raça)");
+        System.out.println("3. Listar todos os pets cadastrados");
+        System.out.println("4. Alterar os dados do pet cadastrado");
+        System.out.println("5. Deletar um pet cadastrado");
+        System.out.println("6. Sair");
+        System.out.print("Digite uma das opções acima: ");
+    }
+
+    // PET REGISTER ====
+    public void register() {
+        System.out.println("\n=== Cadastro do Pet ===");
+        // 1. Qual o nome e sobrenome do pet?
+        String petName = readQuestionAndAnswer();
+        petController.validateValue(petName, ValidateType.NOME);
+
+        // 2. Qual o tipo do pet (Cachorro/Gato)?
+        String type = readQuestionAndAnswer();
+        PetType petType = (PetType) petController.validateValue(type, ValidateType.TIPO);
+
+        // 3. Qual o sexo do animal?
+        String sex = readQuestionAndAnswer();
+        PetSex petSex = (PetSex) petController.validateValue(sex, ValidateType.SEXO);
+
+        //  4 - Qual o nome da Rua que ele foi encontrado?
+        String petStreet = readQuestionAndAnswer();
+        petController.validateValue(petStreet, ValidateType.RUA);
+
+        // 5 - Qual o número da casa?
+        String petHouseNumber = readQuestionAndAnswer();
+        petHouseNumber = (String) petController.validateValue(petHouseNumber, ValidateType.NUMEROCASA);
+
+        // 6 - Qual a cidade?
+        String petCity = readQuestionAndAnswer();
+        petController.validateValue(petCity,  ValidateType.CIDADE);
+
+        // 7. Qual a idade aproximada do pet?
+        String petAge = readQuestionAndAnswer();
+        petAge = (String) petController.validateValue(petAge, ValidateType.IDADE);
+
+        // 8. Qual o peso em kilos aproximado do pet?
+        String petWeight = readQuestionAndAnswer();
+        petWeight = (String) petController.validateValue(petWeight, ValidateType.PESO);
+
+        // 9. Qual a raça do pet?
+        String petBreed = readQuestionAndAnswer();
+        petController.validateValue(petBreed, ValidateType.RACA);
+
+        petController.addPet(petName, petType, petSex, petStreet, petHouseNumber, petCity, petAge, petWeight, petBreed);
+
+        System.out.println("\n Cadastro realizado com sucesso!");
+    }
+
+    private String readQuestionAndAnswer() {
+        System.out.println(petController.getNextQuestion());
+        return scanner.nextLine();
+    }
+    // ===== END REGISTER
+
+    // PET SEARCH ====
+    public List<Pet> searchPet() {
+        Map<SearchCriteria, String> criterias = new HashMap<>();
+        List<SearchCriteria> options = Arrays.asList(SearchCriteria.values());
+
+        System.out.print("Qual o tipo do pet? (Gato/Cachorro)? ");
+        PetType petTypeSearch = (PetType) petController.validateValue(scanner.nextLine(), ValidateType.TIPO);
+
+        do {
+            System.out.println("\n==== Busca de Pet ====");
+            for (int i = 0; i < options.size(); i++) {
+                System.out.println((i + 1) + ". " + options.get(i).getLabel());
+            }
+
+            System.out.printf("Escolha o %d° critério: ", criterias.size() + 1);
+            int criteria = InputService.readInt(scanner);
+
+            if (criteria >= 1 && criteria <= 6) {
+                SearchCriteria selectedCriteria = options.get(criteria - 1);
+                System.out.printf("Digite o valor de %s : ", selectedCriteria);
+                String value = scanner.nextLine();
+                petController.validateValue(value, selectedCriteria.getValidateType());
+
+                criterias.put(selectedCriteria, value);
+
+                if (criterias.size() == 1) {
+                    System.out.print("Deseja selecionar mais um critério? (S/N)");
+                    String res = scanner.nextLine();
+
+                    if (res.charAt(0) == 'N' || res.charAt(0) == 'n') {
+                        break;
+                    }
+                }
+            }
+
+        } while (criterias.size() != 2);
+
+        return petController.searchPet(petTypeSearch, criterias);
+    }
+    // SEARCH END ===
+
+    // LIST PETS ===
+    public void showPetList(List<Pet> pets) {
+        if (pets.isEmpty()) {
+            System.out.println("Não foi encontrado nenhum pet!");
+        } else {
+            String petFormat = "%d. %s - %s - %s, %s, %s - %s - %s - %s\n";
+
+            int id = 1;
+            for (Pet pet : pets) {
+                System.out.printf(petFormat,
+                        id,
+                        pet.getName(),
+                        pet.getSex(),
+                        pet.getAddress().getStreet(), pet.getAddress().getHouseNumber(), pet.getAddress().getCity(),
+                        pet.getAge(),
+                        pet.getWeight(),
+                        pet.getBreed());
+                id++;
+            }
+        }
+    }
+    // END ===
+
+
+    // UPDATE PET ===
+    public void updatePet() {
+        List<Pet> petsFiltered = searchPet();
+
+        showPetList(petsFiltered);
+
+        if (petsFiltered.isEmpty()) {
+            return;
+        }
+
+
+        int petOption = -1;
+        do {
+            System.out.printf("\nEscolha o número do pet na listagem para alterar o cadastro entre (%d-%d): ", 1, petsFiltered.size());
+
+            petOption = getPetOption(petsFiltered, petOption);
+        }
+        while (petOption < 1 || petOption > petsFiltered.size());
+
+        Pet pet = petsFiltered.get(petOption - 1);
+        Path petFile = petController.getPetFile(pet);
+
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(petFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (int i = 0; i < lines.size(); i++) {
+
+            String line = lines.get(i);
+            String actualType = line.substring(0, 1);
+
+
+            String field = ValidateType.getByFileValue(actualType).getType();
+            System.out.print("Deseja alterar o " + field + " (S/N) ?");
+            String answer = scanner.nextLine();
+
+            if (answer.charAt(0) == 'S') {
+                System.out.println(field + " atual " + line.substring(4));
+                System.out.print("Digite o novo " + field + ": ");
+                String newValue = scanner.nextLine();
+                petController.validateValue(newValue, ValidateType.getByFileValue(actualType));
+                String prefix = line.substring(0, line.indexOf("-") + 1);
+                lines.set(i, prefix + " " + newValue);
+            }
+        }
+
+        try {
+            Files.delete(petFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String[] address = lines.get(3).substring(3).split(", ");
+
+        petController.addPet(
+                lines.get(0).substring(3),
+                PetType.valueOf(lines.get(1).substring(4).toUpperCase()),
+                PetSex.valueOf(lines.get(2).substring(4).toUpperCase()),
+                address[0], address[1], address[2],
+                lines.get(4).substring(3),
+                lines.get(5).substring(3),
+                lines.get(6).substring(3)
+
+        );
+    }
+    // END ===
+
+    // DELETE PET ===
+    public void deletePet() {
+        List<Pet> petsFiltered = searchPet();
+
+        showPetList(petsFiltered);
+
+        if (petsFiltered.isEmpty()) {
+            return;
+        }
+
+        int petOption = -1;
+        do {
+            System.out.printf("\nEscolha o número do pet na listagem para deletar (%d-%d): ", 1, petsFiltered.size());
+            petOption = getPetOption(petsFiltered, petOption);
+        } while (petOption < 1 || petOption > petsFiltered.size());
+
+        Pet petWantsDelete = petsFiltered.get(petOption - 1);
+
+        System.out.println("Deseja realmente deletar " + petWantsDelete.getName() + ": (SIM/NAO) ");
+        String confirmation = scanner.nextLine();
+
+        if (confirmation.equalsIgnoreCase("sim")) {
+            Path folder = Paths.get("petsCadastrados");
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(folder)) {
+                for (Path path : directoryStream) {
+                    if (!Files.isDirectory(path)) {
+                        if (path.getFileName().toString().contains(petWantsDelete.getName().toUpperCase())) {
+                            Files.delete(path);
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+    // END ===
+
+    private int getPetOption(List<Pet> petsFiltered, int petOption) {
+        try {
+            petOption = scanner.nextInt();
+
+            if (petOption < 1 || petOption > petsFiltered.size()) {
+                System.out.printf("\nOpção inválida. Digite um número entre %d e %d.\n\n", 1, petsFiltered.size());
+                showPetList(petsFiltered);
+            }
+        } catch (InputMismatchException ignored) {
+            System.out.printf("\nOpção inválida. Digite um número entre %d e %d.\n\n", 1, petsFiltered.size());
+            showPetList(petsFiltered);
+        } finally {
+            scanner.nextLine();
+        }
+        return petOption;
+    }
+}
